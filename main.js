a76b93b2c0a862ff1b0c3653850e16eb5b1c46d6
Guttorf <147665538+Guttorf@users.noreply.github.com>
2023-10-11 22:59:50 +0000

    criei o arquivo main.js

---
+++ main.js
@@ -0,0 +1,14 @@
+// function cronometro (tempo) {
+//     console.log(tempo)
+//     let proximoValor = tempo -1
+//     if(proximoValor > 0){
+//         cronometro(proximoValor)
+//     }
+// }
+// cronometro(10)
+
+// console.log(10)
+
+let repitaOlaMundo = ("Hello, world")
+if(repitaOlaMundo <= 10)
+console.log(repitaOlaMundo)

f6ebb0fb057ce056417777774dac3c2f59c2d376
Guttorf <147665538+Guttorf@users.noreply.github.com>
2023-10-18 00:40:42 +0000

    treinos mongoose

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node 
-
-COPY main.js /home/node/app/
-
-WORKDIR /home/node/app/
-
-COPY . .
-
-VOLUME . /home/node/app/
---
+++ index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const CatModelo = mongoose.model('Cat',{
+    raca: String,
+    idade: Number,
+    cor: String
+})
+
+const CatQueAcheiNaRua = new CatModelo({
+    raca: "Siames",
+    idade: 4,
+    cor: "Marrom"
+})
+
+CatQueAcheiNaRua.save().then(console.log("Gato salvo com sucesso"))

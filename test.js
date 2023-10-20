0503519e7025c25bd0cf00d2cc6fbf5ce009147c
Guttorf <147665538+Guttorf@users.noreply.github.com>
2023-10-20 23:01:07 +0000

    nova function array

---
+++ test.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+const Produto = mongoose.model('Produto',
+{
+    codigo: Number,
+    nome: String,
+    preco: Number
+})
+
+// ****************************************
+
+// function-createProdutos//
+
+async function criaNovoProduto(produto) {
+    await Produto.create(produto)
+}
+
+// criar const array de produtos //
+
+const produtos = [
+    {nome: "calotas", codigo: 10, preco: 140},
+    {nome: "sensor de temperatura", codigo: 11, preco: 27},
+    {nome: "dosensor de fase", codigo: 12, preco: 355}
+]
+async function criaNovoProduto(produto) {
+    while(contador <= tamanho){ 
+    const tamanho = array.length
+    let contador = 0
+    await Produto.create(produto)}
+    contador++
+
+}
+
+criaNovoProduto(p)
+

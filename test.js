5f9dae658932aea210c1a7f7b039856722e1721e
Guttorf <147665538+Guttorf@users.noreply.github.com>
2023-10-21 00:46:23 +0000

    TESTE FUNCTION ASYNC AWAIT FIND

--- test.js
+++ test.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
+//  MODELO
 const Produto = mongoose.model('Produto',
 {
     codigo: Number,
@@ -8,30 +9,49 @@ const Produto = mongoose.model('Produto',
     preco: Number
 })
 
-// ****************************************
+// FUNCOES
 
-// function-createProdutos//
-
+// function-CreateProduto
 async function criaNovoProduto(produto) {
     await Produto.create(produto)
 }
 
-// criar const array de produtos //
+// function-exibetodososprodutos
+async function exibeTodosOsProdutos(){
+    console.log(await Produto.find())
+}
 
+// create novoproduto array
+async function criarNovoProduto(arr) {
+    const tamanho = arr.length;
+    let contador = 0;
+    while(contador <= tamanho){ 
+        await Produto.create(arr[contador])
+     contador++;
+    }
+}
+
+// FUNCAO DELETAUMPRODUTO
+async function deletaProduto(nomeADeletar){
+    await Produto.findOneAndDelete({nome: nomeADeletar})
+}
+
+// async function exibeUmProduto
+async function exibeUmProduto (nomeAProcurar) {
+    console.log(await Produto.findOne({nome: nomeAProcurar}))
+}
+    
+// criar-const-array-de-produtos
 const produtos = [
     {nome: "calotas", codigo: 10, preco: 140},
     {nome: "sensor de temperatura", codigo: 11, preco: 27},
     {nome: "dosensor de fase", codigo: 12, preco: 355}
 ]
-async function criaNovoProduto(produto) {
-    while(contador <= tamanho){ 
-    const tamanho = array.length
-    let contador = 0
-    await Produto.create(produto[contador]
-        )
-    }
-    contador++
 
-}
-
-
+// Chamada-De-Funcao
+    // criarNovoProduto(produtos)
+    // atualizaProduto()
+    // deletaProduto()
+    // exibeTodosOsProdutos()
+    // exibeUmProduto()
+    
